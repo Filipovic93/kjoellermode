d0de0cb574f5bce85f1dc7413033ad46d012c9d5
Filipovic93 <Damii93@hotmail.com>
2017-02-11 13:39:42 +0100

    added new lion in navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', {title: 'Kjøller - mode for mænd'});
+  res.render('index', {title: 'Kjøller - Mode For Mænd'});
 });
 
 router.post('/contact', function (req, res) {
